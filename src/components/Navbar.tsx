6d8e64686da02b532de5648c73ce4c96f0ca05b6
Hairum Qureshi <hqureshi@udel.edu>
2024-05-31 15:22:42 -0400

    created the hamburger navbar for the mobile view and added functionality to it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,49 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function Navbar() {
-	return <nav></nav>;
+	const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
+
+	return (
+		<div
+			className={`w-full h-screen absolute ${
+				!showHamburgerMenu ? "z-0" : "z-10"
+			}`}
+		>
+			<nav className="bg-custom-nav-color w-full p-5 text-white flex items-center justify-center">
+				<div className="text-2xl">
+					<h1>
+						<Link to="/">Tracker</Link>
+					</h1>
+				</div>
+				<div
+					className="ml-auto inline-flex lg:hidden"
+					onClick={() => setShowHamburgerMenu(!showHamburgerMenu)}
+				>
+					{!showHamburgerMenu ? (
+						<FontAwesomeIcon icon={faBars} className="ml-auto text-xl" />
+					) : (
+						<FontAwesomeIcon icon={faX} />
+					)}
+				</div>
+			</nav>
+			{showHamburgerMenu && (
+				<div className="w-full h-40 bg-blue-300 lg:hidden lg:z-0  flex items-center justify-left">
+					<ul className="text-blue-950 text-2xl p-8">
+						<li>
+							<Link to="#">Time Tracker</Link>
+						</li>
+						<li className="mt-2">
+							<Link to="#">Todo List</Link>
+						</li>
+						<li className="mt-2">
+							<Link to="#">Contacts</Link>
+						</li>
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
