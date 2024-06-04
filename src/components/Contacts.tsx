a20fb385f587e587cae38af4525a06242ad9681c
Hairum Qureshi <hqureshi@udel.edu>
2024-06-04 13:33:44 -0400

    created contacts page

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,7 +1,27 @@
 export default function Contacts() {
 	return (
-		<div className="h-screen p-8 text-2xl text-sky-950">
-			Add your contacts here!
+		<div className="p-8 text-sky-950 absolute lg:relative top-16 w-full m-auto lg:w-2/3">
+			<h2 className="text-2xl font-black font-Kanit lg:text-left text-center">
+				ADD YOUR CONTACTS:
+			</h2>
+			<div className="border-2 border-slate-200 mt-2 p-2 w-full box-border rounded-md bg-slate-100">
+				<div>
+					<label>Enter person's name:</label>
+					<input
+						type="text"
+						placeholder="Name"
+						className="border-2 border-slate-400 outline-none w-full p-1 rounded"
+					/>
+				</div>
+				<div className="mt-4">
+					<label>Enter their phone number:</label>
+					<input
+						type="tel"
+						placeholder="Phone Number"
+						className="border-2 border-slate-400 outline-none w-full p-1 rounded"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
