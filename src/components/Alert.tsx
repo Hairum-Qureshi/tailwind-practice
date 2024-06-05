05ce31fad6b5015863a58af46de2cfefb2d77a4f
Hairum Qureshi <hqureshi@udel.edu>
2024-06-05 07:19:07 -0400

    finished working on Alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,22 +1,23 @@
-import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCheck,
+	faCircleExclamation
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ReactNode } from "react";
+import { AlertContent as Props } from "./Contacts";
 
-interface Props {
-	isError: boolean;
-	icon: string;
-	message: string;
-	// children: ReactNode;
-}
-
-export default function Alert({ isError, icon, message }: Props) {
-	return (
-		<div
-			className={`w-full box-border bg-red-600 p-2 rounded-md relative z-10 border-2 text-white border-red-800 -top-3`}
-		>
+export default function Alert({ alertContent }: Props) {
+	return !alertContent.isError ? (
+		<div className="w-full box-border bg-green-600 p-2 rounded-md relative z-10 border-2 text-white border-green-800 -top-3">
+			<div className="flex items-center">
+				<FontAwesomeIcon icon={faCheck} />
+				<p className="ml-2">{alertContent.message}</p>
+			</div>
+		</div>
+	) : (
+		<div className="w-full box-border bg-red-600 p-2 rounded-md relative z-10 border-2 text-white border-red-800 -top-3">
 			<div className="flex items-center">
 				<FontAwesomeIcon icon={faCircleExclamation} />
-				<p className="ml-2">Warning Here</p>
+				<p className="ml-2">{alertContent.message}</p>
 			</div>
 		</div>
 	);
