6bcf78ef91b86cb9a43d0a8d9a2138f58427b449
Hairum Qureshi <hqureshi@udel.edu>
2024-06-15 23:34:55 -0400

    updated route to the money tracker page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
 								<Link to="/contacts">Contacts</Link>
 							</li>
 							<li className="ml-4">
-								<Link to="/money-tracker">Money Tracker</Link>
+								<Link to="/finances">Money Tracker</Link>
 							</li>
 						</ul>
 					</div>
